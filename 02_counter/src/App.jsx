58c6f22a3d4c0b8e946cf0f0ccba27dd98cfa77b
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-11-29 10:59:48 +0530

    interview question

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -5,7 +5,22 @@ function App() {
   const [counter, setCounter] = useState(0);
 
   const addCounter = () => {
-    setCounter(counter => counter + 1);
+    setCounter(counter + 1);
+    //interview quesions
+    // what will happen if we do does it will increase the counter by 5 or what willhappen
+    
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+
+    //answeer - the increment is just by 1 because  useState send all the update to be done on ui in a batch (i,e fiber logic)
+    //React will batch these state updates together and execute them in a single update.
+    
+    //but if you want such functinalty you can use it like this
+    
+    setCounter(prevCounter => prevCounter+1);
+    // prevCOunter here is the latest  updated state which you fetched again
   };
 
   const decreaseCounter = () => {
