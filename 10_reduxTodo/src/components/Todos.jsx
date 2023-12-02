81cf97c8f90960102b0de041a0e18c042d7dd63d
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-12-02 14:33:45 +0530

    todoRedux

--- 10_reduxTodo/src/components/Todos.jsx
+++ 10_reduxTodo/src/components/Todos.jsx
@@ -1,45 +1,88 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import {removeTodo} from '../features/todo/todoSlice'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo, updateTodo } from "../features/todo/todoSlice";
+
+const [editableTodo, setEditableTodo] = useState({ id: null, text: "" });
+
+const handleEdit = (todo) => {
+  setEditableTodo({
+    id: todo.id,
+    text: todo.text,
+  });
+};
+
+const handleSave = () => {
+  dispatch(
+    updateTodo({
+      id: editableTodo.id,
+      text: editableTodo.text,
+    })
+  );
+  setEditableTodo({ id: null, text: "" });
+};
+
+const handleInputChange = (e) => {
+  setEditableTodo({
+    ...editableTodo,
+    text: e.target.value,
+  });
+};
 
 function Todos() {
-    const todos = useSelector(state => state.todos)
-    const dispatch = useDispatch()
+  const todos = useSelector((state) => state.todos); // Assuming 'todo' is the slice name
+  const dispatch = useDispatch();
+
+  const [editTodo, setEditableTodo] = useState({
+    id: null,
+    text: "",
+  });
 
   return (
     <>
-    <div>Todos</div>
-    <ul className="list-none">
+      <div className="text-xl font-bold mb-4">Todos</div>
+      <ul className="list-none">
         {todos.map((todo) => (
           <li
-            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            className="mt-4 flex justify-between items-center bg-gray-900 px-4 py-2 rounded-md"
             key={todo.id}
           >
-            <div className='text-white'>{todo.text}</div>
-            <button
-             onClick={() => dispatch(removeTodo(todo.id))}
-              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
+            {editableTodo.id === todo.id ? (
+              <input
+                type="text"
+                value={editableTodo.text}
+                onChange={handleInputChange}
+                className="w-full border border-gray-400 rounded px-2 py-1"
+              />
+            ) : (
+              <div className="text-white flex-grow">{todo.text}</div>
+            )}
+            {editableTodo.id === todo.id ? (
+              <button
+                onClick={handleSave}
+                className="px-3 py-1 bg-green-600 text-white rounded-md"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
+                Save
+              </button>
+            ) : (
+              <button
+                onClick={() => handleEdit(todo)}
+                className="px-3 py-1 bg-blue-600 text-white rounded-md mr-2"
+              >
+                Edit
+              </button>
+            )}{" "}
+            */
+            <button
+              onClick={() => dispatch(removeTodo(todo.id))}
+              className="px-3 py-1 bg-red-600 text-white rounded-md"
+            >
+              Delete
             </button>
           </li>
         ))}
       </ul>
     </>
-  )
+  );
 }
 
-export default Todos
+export default Todos;
--- 10_reduxTodo/src/features/todo/todoSlice.js
+++ 10_reduxTodo/src/features/todo/todoSlice.js
@@ -1,54 +1,8 @@
-// import {createSlice,nanoid} from '@reduxjs/toolkit'
-
-// const initialState = {
-//     todos:[{id:1,text:"hello world"}]
-// }
-
-// export const todoSlice= createSlice({
-//    name:'todo',
-//    initialState,
-//    reducers:{
-
-//     addTodo:(state,action)=>{
-//         const todo ={
-//             id:nanoid(),
-//             text: action.payload
-//         }
-//         state.todos.push(todo)
-//     },
-//     removeTodo:(state, action )=>{
-//         state.todos = state.todos.filter((todo)=> todo.id!== action.payload)
-//     },
-
-//     upadateTodo:(state,action)=>{
-//     state.todos = state.todos.map((todo)=>todo.id == action.id? {...todo,todo:action.payload }: todo)
-//     }
-
-//    }
-// })
-// export const {addTodo,removeTodo,upadateTodo} = todoSlice.actions
-
-// export default todoSlice.reducer
-
-// // how to create and use store in project 
-
-// // first create a store usign configurestore which taks an object as argument 
-
-// // second create the intialState of the SpeechRecognitionResultList.e they are basic function
-
-// // 3 create a slice using createSlice (dslice is nothng but a bigger reducer for functionality ), 
-// // createSlice methods takes object which contain (name of slice , intialState , reducers)
-// // s
-// // reducres are methods  takes two arguments (state,action) state gives the aacces to current state of store,,
-// // we take any parameters from the actions using(action.payload.parametername)
-
-import {createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-    todos: [{id: 1, text: "Hello world"}]
+    todos: [{ id: 1, text: "Hello world" }]
 }
-
-
 
 export const todoSlice = createSlice({
     name: 'todo',
@@ -56,17 +10,21 @@ export const todoSlice = createSlice({
     reducers: {
         addTodo: (state, action) => {
             const todo = {
-                id: nanoid(), 
+                id: nanoid(),
                 text: action.payload
             }
             state.todos.push(todo)
         },
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         },
+        updateTodo: (state, action) => {
+            const { id, text } = action.payload;
+            state.todos = state.todos.map((todo) => todo.id === id ? { ...todo, text } : todo)
+        }
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions
 
 export default todoSlice.reducer
