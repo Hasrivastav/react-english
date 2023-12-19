7e217ba98bec65143f78ac565ad47b41ed2f6b99
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-12-19 18:56:45 +0530

    login , signup and authlayotu

--- 12_AppwriteMegablogProject/src/components/header/LogoutBtn.jsx
+++ 12_AppwriteMegablogProject/src/components/header/LogoutBtn.jsx
@@ -16,6 +16,7 @@ function LogoutBtn() {
 
   return (
    <button
+   onClick={logoutHandler}
    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
     Logout
    </button>
--- 12_AppwriteMegablogProject/src/components/index.js
+++ 12_AppwriteMegablogProject/src/components/index.js
@@ -1,14 +1,15 @@
-import Header from "./header/Header";
+import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 export{
-    Header ,
+    
      Footer,
      Container,
      Logo,
-     LogoutBtn
+     LogoutBtn,
+     Header
      
 
 }
