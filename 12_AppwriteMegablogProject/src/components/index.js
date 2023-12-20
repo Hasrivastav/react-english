9d09cb17abb229aa3c32234b8a181ed6c83ddefa
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-12-20 23:30:10 +0530

    Post Form

--- 12_AppwriteMegablogProject/src/components/index.js
+++ 12_AppwriteMegablogProject/src/components/index.js
@@ -3,13 +3,15 @@ import Footer from "./Footer/Footer";
 import Container from "./container/container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
+import RTE from './RTE'
 export{
     
      Footer,
      Container,
      Logo,
      LogoutBtn,
-     Header
+     Header,
+     RTE
      
 
 }
---
+++ 12_AppwriteMegablogProject/src/components/post-from/PostForm.jsx
@@ -0,0 +1,29 @@
+import React,{useCallback} from "react";
+import {useForm} from
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//setValue is used toset vales
+//control is used toget control of any form this is the same control passed to the RTE
+//getValuees used to get all the values of the from
