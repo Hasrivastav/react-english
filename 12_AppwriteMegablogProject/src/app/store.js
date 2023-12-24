740a4e59d65ec0ed7a43ef7365f8bd2caefbacd8
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-12-24 09:13:46 +0530

    Mega project complete

--- 12_AppwriteMegablogProject/src/app/store.js
+++ 12_AppwriteMegablogProject/src/app/store.js
@@ -2,7 +2,9 @@ import { configureStore } from '@reduxjs/toolkit'
 import authSlice from '../features/status/authSlice';
 
 export const store = configureStore({
-    reducer: authSlice,
+    reducer: {
+      auth:authSlice,
+    }
   })
 
   export default store;
--- 12_AppwriteMegablogProject/src/components/post-from/PostForm.jsx
+++ 12_AppwriteMegablogProject/src/components/post-from/PostForm.jsx
@@ -1,7 +1,8 @@
 import React, { useCallback } from "react";
 import { useForm } from "react-hook-form";
 import { Button, Input, RTE, Select } from "..";
-import appwriteService from "../../appwrite/config";
+import fileService from "../../appwrite/file";
+import dbServices from "../../appwrite/config";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -13,20 +14,20 @@ export default function PostForm({ post }) {
             content: post?.content || "",
             status: post?.status || "active",
         },
-    });
+    });s
 
     const navigate = useNavigate();
     const userData = useSelector((state) => state.auth.userData);
 
     const submit = async (data) => {
         if (post) {
-            const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
+            const file = data.image[0] ? await fileService.uploadFile(data.image[0]) : null;
 
             if (file) {
-                appwriteService.deleteFile(post.featuredImage);
+                fileService.deleteFile(post.featuredImage);
             }
 
-            const dbPost = await appwriteService.updatePost(post.$id, {
+            const dbPost = await dbServices.updatePost(post.$id, {
                 ...data,
                 featuredImage: file ? file.$id : undefined,
             });
@@ -35,12 +36,12 @@ export default function PostForm({ post }) {
                 navigate(`/post/${dbPost.$id}`);
             }
         } else {
-            const file = await appwriteService.uploadFile(data.image[0]);
+            const file = await fileService.uploadFile(data.image[0]);
 
             if (file) {
                 const fileId = file.$id;
                 data.featuredImage = fileId;
-                const dbPost = await appwriteService.createPost({ ...data, userId: userData.$id });
+                const dbPost = await dbServices.createPost({ ...data, userId: userData.$id });
 
                 if (dbPost) {
                     navigate(`/post/${dbPost.$id}`);
@@ -101,7 +102,7 @@ export default function PostForm({ post }) {
                 {post && (
                     <div className="w-full mb-4">
                         <img
-                            src={appwriteService.getFilePreview(post.featuredImage)}
+                            src={fileService.getFilePreview(post.featuredImage)}
                             alt={post.title}
                             className="rounded-lg"
                         />
@@ -120,9 +121,6 @@ export default function PostForm({ post }) {
         </form>
     );
 }
-
-
-
 
 //setValue is used toset vales
 //control is used toget control of any form this is the same control passed to the RTE
